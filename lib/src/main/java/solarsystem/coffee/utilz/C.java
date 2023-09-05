24b1062d1bd26e17eeb0c47aef8820261e17c50a
IIIzP0III <zP@solarsystem.coffee>
2023-09-04 17:25:53 -0800

    baa plugin created

---
+++ lib/src/main/java/solarsystem/coffee/utilz/C.java
@@ -0,0 +1,27 @@
+package solarsystem.coffee.utilz;
+import org.bukkit.ChatColor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class C {
+    public C() {
+    }
+
+    public static String color(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+
+    public static List<String> colorLines(List<String> lore) {
+        List<String> color = new ArrayList();
+        Iterator var2 = lore.iterator();
+
+        while(var2.hasNext()) {
+            String s = (String)var2.next();
+            color.add(color(s));
+        }
+
+        return color;
+    }
+}
---
+++ lib/src/main/java/solarsystem/coffee/zPbaa.java
@@ -0,0 +1,73 @@
+package solarsystem.coffee;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import solarsystem.coffee.utilz.C;
+
+public class zPbaa extends JavaPlugin {
+
+Player player;
+  ;
+  @Override
+  public void onEnable() {
+    Bukkit.getServer().getConsoleSender().sendMessage("zPbaa enabled");
+  }
+    @Override
+  public void onDisable() {
+
+    Bukkit.getServer().getConsoleSender().sendMessage("zPbaa disabled");
+  }
+
+
+
+
+  @Override
+  public boolean onCommand(CommandSender interpreter, Command cmd, String input, String[] args) {
+
+    player = (Player) interpreter;
+    Server server = interpreter.getServer();
+    String serverName = server.getName();
+
+
+    if (interpreter instanceof Player) {
+
+
+      if (input.equals("baa") || input.equals("boop")) {
+
+        if (args.length != 1) {
+          player.sendMessage(C.color("&6use: /baa or /boop playername"));
+          return false;
+        }
+
+        String Name = args[0];
+
+        Player Target = null;
+        for (Player p : Bukkit.getOnlinePlayers()) {
+          if (p.getName().equalsIgnoreCase(Name)) {
+            Target = p;
+
+          }
+        }
+
+        //Execute Boop
+        if (Target != null) {
+          Target.sendMessage(C.color("&6&l" + player.getName() + "&6 send you " + "&6&l✂ Baaaaaaaaaaaa ✂ "));
+          Target.playSound(Target.getLocation(), Sound.ENTITY_SHEEP_AMBIENT, (float) 40.0, (float) 18.0);
+          player.sendMessage(C.color("&6&l| &6&l✂ Baaaa ✂ &4&l| &6&l" + Target.getName() + " &6&l <3 &6&l| "));
+        } else {
+          player.sendMessage(ChatColor.LIGHT_PURPLE + "Target Player not found");
+        }
+        return true;
+      }
+
+
+    }
+    return true;
+  }
+  }
